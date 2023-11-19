08ffeb1490c1629dd567f0c39bf6247ab182e5c3
pochopsp <pasquale.1996@gmail.com>
2023-11-19 10:16:41 +0100

    Add param in string_functions doc

--- common/string_functions.h
+++ common/string_functions.h
@@ -11,6 +11,7 @@
  *  in the uint16_t pointer given in input. Otherwise, false is returned and the pointer will remain untouched.
  *
  *  @param res The pointer to the uint16_t to store the number into.
+ *  @param string The pointer to the string to read the number from.
  *  @return true if conversion was made successfully, false in case of error.
  */
 bool string_to_uint16(const char *string, uint16_t *res);
@@ -22,6 +23,7 @@ bool string_to_uint16(const char *string, uint16_t *res);
  *  in the uint8_t pointer given in input. Otherwise, false is returned and the pointer will remain untouched.
  *
  *  @param res The pointer to the uint8_t to store the number into.
+ *  @param string The pointer to the string to read the number from.
  *  @return true if conversion was made successfully, false in case of error.
  */
 bool string_to_uint8(const char *string, uint8_t *res);

376222254407e460ccdca3e28481064c5b66a4a3
pochopsp <pasquale.1996@gmail.com>
2023-11-19 10:15:40 +0100

    Extract common code in string_functions

--- common/string_functions.c
+++ common/string_functions.c
@@ -3,24 +3,26 @@
 #include <errno.h>
 #include <stdlib.h>
 
-bool string_to_uint16(const char *string, uint16_t *res) {
+long strtol_with_limit(const char *string, int limit){
     char *end;
     errno = 0;
     long val = strtol(string, &end, 10);
-    if (errno || end == string || *end != '\0' || val < 0 || val >= 0x10000) {
-        return false;
+    if (errno || end == string || *end != '\0' || val < 0 || val >= limit) {
+        return -1;
     }
+    return val;
+}
+
+bool string_to_uint16(const char *string, uint16_t *res) {
+    long val = strtol_with_limit(string, 0x10000);
+    if(val == -1) return false;
     *res = (uint16_t)val;
     return true;
 }
 
 bool string_to_uint8(const char *string, uint8_t *res) {
-    char *end;
-    errno = 0;
-    long val = strtol(string, &end, 10);
-    if (errno || end == string || *end != '\0' || val < 0 || val >= 0x10000) {
-        return false;
-    }
+    long val = strtol_with_limit(string, 0xFF);
+    if(val == -1) return false;
     *res = (uint8_t)val;
     return true;
 }

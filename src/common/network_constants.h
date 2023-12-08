0163d39ba1ea1dc0d1c51f4f22268470e19b1b47
pochopsp <pasquale.1996@gmail.com>
2023-12-08 11:11:25 +0100

    Add const keyword on src/common/network_constants

--- src/common/network_constants.h
+++ src/common/network_constants.h
@@ -4,15 +4,15 @@
 /** @brief Lower limit for TCP and UDP port number for private use, see
  * https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers#Dynamic,_private_or_ephemeral_ports
  */
-extern int PORT_LWRBND_LIMIT;
+extern const int PORT_LWRBND_LIMIT;
 
 /** @brief Upper limit for TCP and UDP port number for private use, see
  * https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers#Dynamic,_private_or_ephemeral_ports
  */
-extern int PORT_UPPBND_LIMIT;
+extern const int PORT_UPPBND_LIMIT;
 
 /** @brief IPv4 length in decimal dot notation
  */
-extern int IPV4_DOTNTN_LENGTH;
+extern const int IPV4_DOTNTN_LENGTH;
 
 #endif // NETWORK_CONSTANTS_H

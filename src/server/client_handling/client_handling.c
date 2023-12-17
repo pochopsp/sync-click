eb4f3b8aacc5712e5d6ccb4464d546183011b1e6
pochopsp <pasquale.1996@gmail.com>
2023-12-17 22:47:44 +0100

    Add client thread handing

---
+++ src/server/client_handling/client_handling.c
@@ -0,0 +1,8 @@
+#include "./client_handling.h"
+
+#include <stdlib.h>
+
+void* client_thread_handler(void *args){
+
+    return NULL;
+}
---
+++ src/server/client_handling/client_handling.h
@@ -0,0 +1,7 @@
+#ifndef CLIENT_HANDLING_H
+#define CLIENT_HANDLING_H
+
+//connected clients threads' start routine
+void* client_thread_handler(void *args);
+
+#endif // CLIENT_HANDLING_H

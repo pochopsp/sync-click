c23133e9efbe13d9a7a54e611db2416c0f31cb8b
pochopsp <pasquale.1996@gmail.com>
2023-12-24 20:42:38 +0100

    Implement client_thread_handler

--- src/server/client_handling/client_handling.c
+++ src/server/client_handling/client_handling.c
@@ -1,8 +1,52 @@
 #include "./client_handling.h"
+#include "../../macros.h"
 
+#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+bool time_to_send_maxrtt = false;
+unsigned long max_rtt = 0;
 
 void* client_thread_handler(void *args){
 
+    struct clientargs *clientargs = args;
+    int client_sock_fd = clientargs->client_sock_fd;
+    unsigned long client_rtt = clientargs->client_rtt;
+    pthread_mutex_t *m = clientargs->mutex;
+    pthread_cond_t *c = clientargs->cond;
+    free(clientargs); // free the memory previously allocated for the struct
+
+    // 1st step -- send the rtt for its own connection to this client "MY_RTT_CMD <rtt>"
+    char str_rtt[256] = {'\0'};
+    strcat(str_rtt, MY_RTT_CMD);
+    strcat(str_rtt, " ");
+    sprintf(str_rtt+strlen(str_rtt), "%lu", client_rtt);
+    write(client_sock_fd, str_rtt, strlen(str_rtt));
+
+    // 2nd step -- send the max rtt among all clients to this client "MAX_RTT_CMD <rtt>"
+    pthread_mutex_lock(m);
+    while (!time_to_send_maxrtt){
+        pthread_cond_wait(c, m);
+    }
+    pthread_mutex_unlock(m);
+    memset(str_rtt, 0, sizeof(str_rtt));
+    strcat(str_rtt, MAX_RTT_CMD);
+    strcat(str_rtt, " ");
+    sprintf(str_rtt+strlen(str_rtt), "%lu", max_rtt);
+    write(client_sock_fd, str_rtt, strlen(str_rtt));
+
+    // 3rd step -- loop to wait for click requests
+    while(true){
+        pthread_mutex_lock(m);
+        pthread_cond_wait(c, m);
+        write(client_sock_fd, CLK_CMD, strlen(CLK_CMD));
+        pthread_mutex_unlock(m);
+    }
+
+    close(client_sock_fd);
     return NULL;
 }
--- src/server/client_handling/client_handling.h
+++ src/server/client_handling/client_handling.h
@@ -1,10 +1,18 @@
 #ifndef CLIENT_HANDLING_H
 #define CLIENT_HANDLING_H
 
+#include <pthread.h>
+#include <stdbool.h>
+
+extern bool time_to_send_maxrtt;
+extern unsigned long max_rtt;
+
 //a structure we use to pass arguments to threads created for clients connections.
 struct clientargs{
 	int client_sock_fd;
     unsigned long client_rtt;
+    pthread_mutex_t *mutex;
+	pthread_cond_t *cond;
 };
 
 //connected clients threads' start routine
